c2ad4bcecb5db7b7efd0d8a186b6b7b571f56d11
Alejandro Romero <aromero@alorahealth.com>
2025-04-04 17:00:53 -0300

    update and fixed code

--- Products.Api/Database/ApplicationDbContext.cs
+++ Products.Api/Database/ApplicationDbContext.cs
@@ -8,10 +8,9 @@ public sealed class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
-        //AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        
     }
-
-    //public DbSet<ProductOlder> Product { get; set; }
+    
     public DbSet<Products.Api.Entities.Products> Products { get; set; }
     public DbSet<SubCategory> SubCategories { get; set; }
 
--- Products.Api/Program.cs
+++ Products.Api/Program.cs
@@ -72,6 +72,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.ApplyMigrations();error on connection
 }
 
 app.MapProductsEndpoints();
